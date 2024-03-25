31b96947ea84574aac2e30943a0dc28217019f6d
Alex Garcia <alexxelacr7@gmail.com>
2024-03-24 19:13:45 -0700

    New update

---
+++ BrickByBrick/src/application/Asset.java
@@ -0,0 +1,36 @@
+package application;
+
+public class Asset {
+	
+	private String name;
+	private String category;
+	private String location;
+	
+	
+	// Setter and Getter methods
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }  
+
+}
